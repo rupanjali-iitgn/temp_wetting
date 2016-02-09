17044c26b0c05976f4d3c5c737e53cc6640b9db6
kaustubhsunilrane <kaustubh.rane21@gmail.com>
2016-02-09 10:42:45 +0100

    Project to design solid surfaces with desired temperature dependence

---
+++ temp_wetting.mm
@@ -0,0 +1,174 @@
+<map version="docear 1.1" dcr_id="1450463180897_6qgauh4sd8sqpqhzhhkavixlk" project="151B657B699COWUDKPJMBV73Y7CUGM2FE9PK" project_last_home="file:/home/local/CSI/kr78neqo/Dropbox/Research/Projects/iitgn/proposals/temp_wetting/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Designing solid surfaces with desired temperature dependence of wetting behavior" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1450463414017"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10"/>
+<node TEXT="Scientific rationale" POSITION="right" ID="ID_268635931" CREATED="1450463418954" MODIFIED="1450465512918" HGAP="40" VSHIFT="-100">
+<edge COLOR="#ff0000"/>
+<node TEXT="Applications" ID="ID_1616607157" CREATED="1450465178098" MODIFIED="1450465182101">
+<node TEXT="Wetting engines" ID="ID_564676847" CREATED="1450465268042" MODIFIED="1450465273013"/>
+<node TEXT="Micro or nanofluidics" ID="ID_1321830436" CREATED="1450465273483" MODIFIED="1450465284586"/>
+<node TEXT="Miscellaneous" ID="ID_278738019" CREATED="1450465285002" MODIFIED="1450465288383">
+<node TEXT="Catalysis" ID="ID_1903445769" CREATED="1450636538222" MODIFIED="1450636546808"/>
+<node TEXT="Understanding Hysterisis" ID="ID_731793233" CREATED="1450721988728" MODIFIED="1450722002009"/>
+</node>
+</node>
+<node TEXT="Current technical capabilities" ID="ID_235950698" CREATED="1450465182856" MODIFIED="1450465252854">
+<node TEXT="Experimental" ID="ID_1442355972" CREATED="1450465446596" MODIFIED="1450465455105"/>
+<node TEXT="Theoretical" ID="ID_474314366" CREATED="1450465455496" MODIFIED="1450465473394"/>
+<node TEXT="Computational" ID="ID_734735562" CREATED="1450465473940" MODIFIED="1450465483442"/>
+</node>
+</node>
+<node TEXT="Background" POSITION="right" ID="ID_1110611260" CREATED="1450463704236" MODIFIED="1451645237036" HGAP="50" VSHIFT="-290">
+<edge COLOR="#00ffff"/>
+<node TEXT="Research status (international)" ID="ID_1392626561" CREATED="1450463444390" MODIFIED="1450463704312" MOVED="1450463704325">
+<node TEXT="Experimental" ID="ID_869146050" CREATED="1450521720833" MODIFIED="1450521725991">
+<node TEXT="Work with flat interfaces and chemical functionalization: Neumann et al." ID="ID_121688724" CREATED="1450638836342" MODIFIED="1450638865734"/>
+<node TEXT="Work with microcalorimetry" ID="ID_1314084600" CREATED="1450638866409" MODIFIED="1450638909789"/>
+<node TEXT="Enthalpy-entropy compensation or &quot;independence&quot; of the two" ID="ID_30644458" CREATED="1450640781570" MODIFIED="1450723771859"/>
+</node>
+<node TEXT="Computational" ID="ID_1521593594" CREATED="1450521733895" MODIFIED="1451329938951" MOVED="1451329946530">
+<node TEXT="Work from Errington group" ID="ID_1690050024" CREATED="1451329816407" MODIFIED="1451330002869">
+<node TEXT="Systems that were studied" ID="ID_473109498" CREATED="1451330160755" MODIFIED="1451330176423"/>
+<node TEXT="How was it explained" ID="ID_1487682070" CREATED="1451330176841" MODIFIED="1451330206728"/>
+</node>
+<node TEXT="Work from Horsch group" ID="ID_980454220" CREATED="1451330134912" MODIFIED="1451330221246"/>
+<node TEXT="Work of Haiping Fang group on the effect of lattice structure" ID="ID_1854553780" CREATED="1451759677527" MODIFIED="1451759724170"/>
+<node TEXT="DFT calculations from Berim and Ruckenstein" ID="ID_314119525" CREATED="1451331352328" MODIFIED="1451331429660"/>
+</node>
+<node TEXT="Theoretical" ID="ID_1942058277" CREATED="1450521726409" MODIFIED="1450521733396">
+<node TEXT="Importance of correlations along the interface" ID="ID_1905060815" CREATED="1450723775466" MODIFIED="1450723796013">
+<node TEXT="How it is affected by the temperature (via scaling laws)" ID="ID_801342251" CREATED="1451033423394" MODIFIED="1451033449101"/>
+<node TEXT="How they affect interfacial free energies" ID="ID_1964284085" CREATED="1451040811043" MODIFIED="1451040849013"/>
+<node TEXT="How they affect the temperature dependence" ID="ID_751345006" CREATED="1451040849480" MODIFIED="1451040868468"/>
+</node>
+<node TEXT="Generality of this phenomenon and its physics for all surfaces and fluids" ID="ID_1590536878" CREATED="1450952713452" MODIFIED="1450952760760"/>
+<node TEXT="Factors that can influence transverse correlations" ID="ID_398274930" CREATED="1451039960690" MODIFIED="1451040009883">
+<node TEXT="Which of these predominantly influence gamma_sl" ID="ID_580082896" CREATED="1451040895183" MODIFIED="1451040932636"/>
+<node TEXT="Which predominantly influence dgamma_sl/dT" ID="ID_874559696" CREATED="1451040933023" MODIFIED="1451040960512"/>
+</node>
+</node>
+<node TEXT="Solvation and capillary waves" ID="ID_19652608" CREATED="1451331679005" MODIFIED="1451331908991">
+<node TEXT="Effect of capillary waves on solvation and what does it mean wrt interfacial free energies" ID="ID_106467715" CREATED="1451331927499" MODIFIED="1451332373179"/>
+<node TEXT="Effect of solute on capillary waves and what does it mean to temperature dependence" ID="ID_1103070320" CREATED="1451332373785" MODIFIED="1451332413102"/>
+<node TEXT="How this may connect to solid surfaces" ID="ID_276381896" CREATED="1451332413595" MODIFIED="1451332637790"/>
+</node>
+</node>
+<node TEXT="Research status (national)" ID="ID_374576713" CREATED="1450463480339" MODIFIED="1450463751185" VSHIFT="-30">
+<node TEXT="Chinmay&apos;s group compares sessile drop and calorimetric approaches" ID="ID_135785427" CREATED="1451411553988" MODIFIED="1451411593606">
+<node TEXT="Useful for justifyng the microcalorimetry" ID="ID_1532965186" CREATED="1451411598074" MODIFIED="1451411637726"/>
+<node TEXT="Can coating density affect the temperature dependence?" ID="ID_838488932" CREATED="1451413666164" MODIFIED="1451413682529"/>
+<node TEXT="Can mix of different coatings be tested to &quot;tune&quot; hydrophilic(phobic) nature?" ID="ID_1401479645" CREATED="1451414701521" MODIFIED="1451414748071"/>
+</node>
+</node>
+<node TEXT="Novelty of our work" ID="ID_1979164963" CREATED="1450463512939" MODIFIED="1450463773709">
+<node TEXT="How realistic our models should be?" ID="ID_1997632539" CREATED="1450723698983" MODIFIED="1450723721327"/>
+<node TEXT="Exploiting transverse correlations" ID="ID_625214648" CREATED="1450978903083" MODIFIED="1450978915289"/>
+<node TEXT="New ways of studying wetting transitions for technologically relevant systems via experiments" ID="ID_1607535616" CREATED="1451034388431" MODIFIED="1451591552963" VSHIFT="-70">
+<node TEXT="Emphasis on trends with a particular order parameter rather than measurements" ID="ID_825518109" CREATED="1451497913869" MODIFIED="1451497939361"/>
+<node TEXT="Is it possible to create &quot;a set of simple models&quot; to consider a porous structure?" FOLDED="true" ID="ID_1028951925" CREATED="1451504831514" MODIFIED="1451504863549">
+<node TEXT="New possible project" ID="ID_1602351942" CREATED="1451504899903" MODIFIED="1451504907745"/>
+<node TEXT="Alternatives to characterizing surface with Fowkes equation" ID="ID_714004449" CREATED="1451577465346" MODIFIED="1451577498448"/>
+</node>
+</node>
+<node TEXT="Considering theories developed for flat surfaces in the context of more realistic and applied topic of porous powders" ID="ID_30899692" CREATED="1451411645810" MODIFIED="1451591704305" VSHIFT="-10"/>
+</node>
+</node>
+<node TEXT="Work plan" POSITION="left" ID="ID_1615838882" CREATED="1450463946512" MODIFIED="1451645257644" HGAP="-650" VSHIFT="390">
+<edge COLOR="#ffff00"/>
+<node TEXT="Methods" ID="ID_1509493354" CREATED="1450463995842" MODIFIED="1450464012873" HGAP="-150">
+<node TEXT="Experimental" ID="ID_176697614" CREATED="1451641322937" MODIFIED="1451645263973" HGAP="-215" VSHIFT="3">
+<node TEXT="Systems" ID="ID_222214319" CREATED="1451643485617" MODIFIED="1451644496558" HGAP="-150" VSHIFT="-50">
+<node TEXT="&quot;Packed-bed&quot; and flat-surface wetting studies on crystalline materials" ID="ID_890750199" CREATED="1451643885922" MODIFIED="1452105603230" HGAP="-479" VSHIFT="-1"/>
+</node>
+<node TEXT="Method of manipulating the hydrophilicity and hydrophocity" ID="ID_1528992532" CREATED="1452105611908" MODIFIED="1452105643083" HGAP="-450" VSHIFT="-30">
+<node TEXT="Radiation?" ID="ID_630039349" CREATED="1452105644893" MODIFIED="1452105656062" HGAP="-420" VSHIFT="-20"/>
+<node TEXT="Chemical treatment?" ID="ID_1162892636" CREATED="1452105657425" MODIFIED="1452105691841" HGAP="-470" VSHIFT="-10"/>
+<node TEXT="Coating?" ID="ID_1113661769" CREATED="1452105675396" MODIFIED="1452105687061" HGAP="-410" VSHIFT="10"/>
+</node>
+<node TEXT="Contact angle determination on flat surface" ID="ID_768785223" CREATED="1451644521916" MODIFIED="1451644840175" HGAP="-360" VSHIFT="-20"/>
+<node TEXT="Immersion calorimetry" ID="ID_1121131843" CREATED="1451644114030" MODIFIED="1451644160602" HGAP="-233" VSHIFT="20"/>
+<node TEXT="Surface area determination by some methods" ID="ID_627706002" CREATED="1451644164129" MODIFIED="1451644213389" HGAP="-377" VSHIFT="26"/>
+</node>
+<node TEXT="computational" ID="ID_872435313" CREATED="1451641343605" MODIFIED="1451645268474" HGAP="-200" VSHIFT="60">
+<node TEXT="Systems" ID="ID_1103117492" CREATED="1451644234090" MODIFIED="1451645216336" HGAP="-168" VSHIFT="-59">
+<node TEXT="Models of different crustal faces like 100, 001 etc." ID="ID_190203064" CREATED="1451645272210" MODIFIED="1452105559844" HGAP="-455"/>
+<node TEXT="Solutes positioned at l-v interfaces" ID="ID_808283525" CREATED="1452105529857" MODIFIED="1452105553560" HGAP="-380" VSHIFT="10"/>
+</node>
+<node TEXT="Simulations of different types of crystal faces. (These will be validated against the experimental studies)" ID="ID_1619461814" CREATED="1452105774948" MODIFIED="1452106181294" HGAP="-716" VSHIFT="-10"/>
+<node TEXT="Simulations with solutes positioned at l-v interfaces" ID="ID_1547495769" CREATED="1452105826761" MODIFIED="1452105968256" HGAP="-430" VSHIFT="30"/>
+<node TEXT="Theoretical analysis connecting the above two types of systems" ID="ID_1538917616" CREATED="1452105968887" MODIFIED="1452106124526" HGAP="-500" VSHIFT="20"/>
+</node>
+</node>
+<node TEXT="Time schedule" ID="ID_581300306" CREATED="1450464034600" MODIFIED="1450464045378" HGAP="-180" VSHIFT="10"/>
+<node TEXT="Use of research outcome" ID="ID_1195711759" CREATED="1450464054401" MODIFIED="1450464095942" HGAP="-240" VSHIFT="30"/>
+<node TEXT="Risk analysis" ID="ID_1268337594" CREATED="1450464106849" MODIFIED="1450464132710" HGAP="-170" VSHIFT="20"/>
+<node TEXT="" ID="ID_392987275" CREATED="1450978922012" MODIFIED="1450978922012"/>
+</node>
+<node TEXT="Expertise and preliminary results" POSITION="left" ID="ID_3125732" CREATED="1450464165608" MODIFIED="1450464737474" HGAP="-749" VSHIFT="20">
+<edge COLOR="#7c0000"/>
+<node TEXT="Computational" ID="ID_1455861183" CREATED="1452240981411" MODIFIED="1452244552181" HGAP="-260" VSHIFT="-30">
+<node TEXT="For a ghost solute, how ensemble averages of Equation (25) of cap_wave_field1 paper change with temperature for bulk liquid and interface" ID="ID_485443609" CREATED="1452241010747" MODIFIED="1452242663040" HGAP="-695" VSHIFT="-30"/>
+<node TEXT="How the contribution from capillary waves (deldelmu) affected by temperature?" ID="ID_577294012" CREATED="1452243654482" MODIFIED="1452244273750" HGAP="-600" VSHIFT="-20"/>
+<node TEXT="Studies with 2 solutes with different separation at bulk and interface: 1) How coeffs of Equation (25) ar affected by distance and 2) How they are affected by temperature" ID="ID_1474088810" CREATED="1452244265030" MODIFIED="1452244504848" HGAP="-700" VSHIFT="10"/>
+<node TEXT="Temperature dependence of wetting behavior on a lattice surface different than the one earlier observed" ID="ID_1367637509" CREATED="1452244564289" MODIFIED="1452244649061" HGAP="-696" VSHIFT="40"/>
+</node>
+<node TEXT="Experimental" ID="ID_1178677590" CREATED="1452244680557" MODIFIED="1452244691304" HGAP="-200" VSHIFT="40">
+<node TEXT="measuring contact angles and temperature dependence on flat surfaces" ID="ID_1995511474" CREATED="1452244692670" MODIFIED="1452245048278" HGAP="-584" VSHIFT="-50"/>
+<node TEXT="Measurin wetting properties in porous beds" ID="ID_853592622" CREATED="1452245027800" MODIFIED="1452245088328" HGAP="-400" VSHIFT="-10"/>
+<node TEXT="Analysis of crystal faces and working with crystals using processes like milling" ID="ID_1635488399" CREATED="1452245089411" MODIFIED="1452245147128" HGAP="-564" VSHIFT="30"/>
+</node>
+</node>
+<node TEXT="Money and technical resources" POSITION="left" ID="ID_1082352398" CREATED="1450464823139" MODIFIED="1450464842337" HGAP="-730" VSHIFT="40">
+<edge COLOR="#00007c"/>
+</node>
+</node>
+</map>
